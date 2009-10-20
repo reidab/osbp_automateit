a7c9970d2148515077f372971af549897533ed3a
Reid Beels <mail@reidbeels.com>
2009-10-19 18:17:24 -0700

    Added recipe to install virtualenv

--- recipes/all.rb
+++ recipes/all.rb
@@ -52,6 +52,7 @@ unless tagged?(:standby)
   invoke 'base_php5'
   invoke 'base_ruby_enterprise'
   invoke 'base_passenger' # Relies on base_apache, base_ruby_enterprise
+  invoke 'base_python_virtualenv'
   # invoke 'base_memcached'
 
   # Customizations
---
+++ recipes/base_python_virtualenv.rb
@@ -0,0 +1,5 @@
+# install VirtualEnv
+
+package_manager.install <<-HERE, :with => :egg
+  virtualenv
+HERE
