3c3d25b1e506974d2fce6b155b01c55da1f280f1
Reid Beels <mail@reidbeels.com>
2009-10-20 01:14:50 -0700

    Finished install recipe for mod_wsgi

--- recipes/base_apache_mod_wsgi.rb
+++ recipes/base_apache_mod_wsgi.rb
@@ -1,8 +1,25 @@
-wsgi_version = '2.6'
+modified = false
 
-sh "svn co http://modwsgi.googlecode.com/svn/tags/mod_wsgi-#{wsgi_version} /usr/src/mod_wsgi-#{wsgi_version}"
-cd "/usr/src/mod_wsgi-#{wsgi_version}" do
-  sh './configure'
-  sh 'make'
-  sh 'make install'
+wsgi_version = '2.6'
+tag = "mod_wsgi-#{wsgi_version}"
+
+
+unless File.exist?('/usr/lib/apache2/modules/mod_wsgi.so')
+  mktempdircd do
+    sh "svn co http://modwsgi.googlecode.com/svn/tags/#{tag} #{tag}"
+    cd tag do
+      sh './configure'
+      sh 'make'
+      sh 'make install'
+      modified = true
+    end
+  end
 end
+
+modified |= render :text => <<-HERE, :to => "/etc/apache2/mods-available/mod_wsgi.load"
+LoadModule wsgi_module modules/mod_wsgi.so
+HERE
+
+modified |= apache_manager.enable_module "mod_wsgi
+apache_manager.reload if modified"
+
