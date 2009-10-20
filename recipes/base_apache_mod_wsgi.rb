e2698ad385a7e24ac9e1fd7bc864a3bc4afa718d
Reid Beels <mail@reidbeels.com>
2009-10-20 01:19:41 -0700

    Include full path to mod_wsgi.so

--- recipes/base_apache_mod_wsgi.rb
+++ recipes/base_apache_mod_wsgi.rb
@@ -2,9 +2,10 @@ modified = false
 
 wsgi_version = '2.6'
 tag = "mod_wsgi-#{wsgi_version}"
+mod_wsgi_so = "/usr/lib/apache2/modules/mod_wsgi.so"
 
 
-unless File.exist?('/usr/lib/apache2/modules/mod_wsgi.so')
+unless File.exist?(mod_wsgi_so)
   mktempdircd do
     sh "svn co http://modwsgi.googlecode.com/svn/tags/#{tag} #{tag}"
     cd tag do
@@ -17,9 +18,8 @@ unless File.exist?('/usr/lib/apache2/modules/mod_wsgi.so')
 end
 
 modified |= render :text => <<-HERE, :to => "/etc/apache2/mods-available/mod_wsgi.load"
-LoadModule wsgi_module modules/mod_wsgi.so
+LoadModule wsgi_module #{mod_wsgi_so}
 HERE
 
 modified |= apache_manager.enable_module "mod_wsgi"
 apache_manager.reload if modified
-
