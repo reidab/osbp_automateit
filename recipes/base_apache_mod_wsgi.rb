7871559c55f89bb7b3efcd90889248ab43724723
Reid Beels <mail@reidbeels.com>
2009-10-19 18:35:15 -0700

    Updated mod_wsgi svn url to use tags

--- recipes/base_apache_mod_wsgi.rb
+++ recipes/base_apache_mod_wsgi.rb
@@ -1,6 +1,6 @@
 wsgi_version = '2.6'
 
-sh "svn co http://modwsgi.googlecode.com/svn/branches/mod_wsgi-#{wsgi_version} /usr/src/mod_wsgi-#{wsgi_version}"
+sh "svn co http://modwsgi.googlecode.com/svn/tags/mod_wsgi-#{wsgi_version} /usr/src/mod_wsgi-#{wsgi_version}"
 cd "/usr/src/mod_wsgi-#{wsgi_version}" do
   sh './configure'
   sh 'make'
